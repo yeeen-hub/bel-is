22eb1ba2e69aa433347cb13e8250caf4a141a6bb
Elly Chwe <ellychwe@example.com>
2026-04-15 09:48:46 +0800

    Add Virtual Tour folders and files such as Sidebar, BelisLayout, HomeLayout, Home, LocationView, PanoramaViewer, locations, and index

---
+++ resources/js/Layouts/Virtual Tour/BelisLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex h-screen">
+    <Sidebar />
+    <main class="flex-1">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Sidebar from '@/Components/Sidebar.vue'
+</script>
---
+++ resources/js/Pages/Virtual Tour/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="text-2xl font-bold">Home</h1>
+</template>
+
+<script setup>
+import HomeLayout from '@/Layouts/Virtual Tour/HomeLayout.vue'
+</script>
